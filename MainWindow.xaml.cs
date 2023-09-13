af081e417dc6f4296d3596af313c1b722dab7df8
csndgy <73069087+csndgy@users.noreply.github.com>
2023-09-13 13:02:21 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace operatorokokokokokokkok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Muvelet> feladatok;
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+        private void btnKiir_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog beolvasas = new OpenFileDialog();
+            if (beolvasas.ShowDialog() == true)
+            {
+                // lblText.Content = File.ReadAllText(beolvasas.FileName);
+                FileBeolvasas(beolvasas.FileName);
+                lblText.Content = "Kifejezések száma: " + feladatok.Count();
+                lblTextMasodik.Content = "Kifejezések maradékos osztásssal: " + feladatok.Count(x => x.Operat == "mod");
+                Negyes();
+                Otos();
+                Hatos();
+            }
+            else
+            {
+                Console.WriteLine("yXXDDD");
+            }
+        }
+
+        
+
+        public void FileBeolvasas(string fajlNev)
+        {
+            feladatok = new List<Muvelet>();
+            foreach (var sor in File.ReadAllLines(fajlNev))
+            {
+                int a = int.Parse(sor.Split()[0]);
+                int b = int.Parse(sor.Split()[2]);
+                string o = sor.Split()[1];
+                feladatok.Add(new Muvelet(a, b, o));
+            }
+        }
+        public void Negyes()
+        {
+            if (feladatok.Any(x => x.Operat == "div") )
+            {
+                lblTextHarmadik.Content = "Van ilyen kifejezés!";
+            }
+            else
+            {
+                lblTextHarmadik.Content = "Szeva";
+            }
+        }
+
+        public void Otos()
+        {
+            int mod = feladatok.Count(x => x.Operat == "mod");
+            int per = feladatok.Count(x => x.Operat == "/");
+            int div = feladatok.Count(x => x.Operat == "div");
+            int min = feladatok.Count(x => x.Operat == "-");
+            int csill = feladatok.Count(x => x.Operat == "*");
+            int plus = feladatok.Count(x => x.Operat == "+");
+            lblTextNegyedik.Content = "Statisztika" + "\n" + "mod -> " + 
+                mod  + " db\n/ -> " + per +" db\ndiv -> " + div
+                +" db\n- -> "+ min + " db\n* -> " +csill + " db\n+ -> " + plus;
+        }
+
+        public void Hatos()
+        {
+
+        }
+    }
+}
---
+++ Muvelet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorokokokokokokkok
+{
+    internal class Muvelet
+    {
+        int operandA, operandB;
+        string operat;
+
+        public Muvelet(int operandA, int operandB, string operat)
+        {
+            this.operandA = operandA;
+            this.operandB = operandB;
+            this.operat = operat;
+        }
+
+        public int OperandA { get => operandA; set => operandA = value; }
+        public int OperandB { get => operandB; set => operandB = value; }
+        public string Operat { get => operat; set => operat = value; }
+    }
+
+    
+}
